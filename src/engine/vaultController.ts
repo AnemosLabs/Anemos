7fb52e01234f8fb486763d6374e00f579af73973
Anemos <anemoslabs@gmail.com>
2025-05-31 14:48:35 +0930

    added signal threshold constant to vault logic

--- src/engine/vaultController.ts
+++ src/engine/vaultController.ts
@@ -3,7 +3,7 @@ import { calculateSignalScore } from "../utils/scoreCalc";
 
 export function getEligibleHolders(vault: Vault, holders: Holder[]) {
   return holders
-    .filter(h => h.holdingDays >= vault.minDays)
+    .filter(h => h.holdingDays >= vault.minDays && calculateSignalScore(h) >= SIGNAL_THRESHOLD)
     .map(h => ({
       wallet: h.address,
       reward: calculateSignalScore(h) * vault.emissionRate
